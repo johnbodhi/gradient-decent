c9e748c8a96dbad8c1efb4d1ee462aa6c41575ef
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2023-08-02 20:15:43 -0700

    Update

--- histGradient_V1/MATLAB Code/runningAverage.m
+++ histGradient_V1/MATLAB Code/runningAverage.m
@@ -1,10 +1,12 @@
 function [ RA ] = runningAverage( dataSet )
 
-    global imageLength
+    global classType classGroups imageLength
+
+    N = size(classType,2)*sizze(classGroups,2); N = M;
     
     % Unsupervised training.
 
-    for k = 1:1:size(dataSet,2)
+    for k = 1:1:size(dataSet,2)-1
         for i = 1:imageLength:size(dataSet,1)
 
             A(i,:,k) = hist(dataSet((i-1)*imageLength+1:i*imageLength,k));
@@ -13,105 +15,106 @@ function [ RA ] = runningAverage( dataSet )
 
     while ( A ~= 0 )
 
-        for k = 1:1:size(A,3)
-            for j = 1:1:size(dataSet,2)
-                for ii = 1:1:size(A,1)
+        
+        if( size(A,1) > N && size(A,2) > M )
+
+            for k = 1:1:size(A,3)        
+                for j = 1:1:size(A,1)
                     for i = 1:1:size(A,1)
         
-                        B(i,ii,j,k) = norm( ( A(ii,:,k) - A(i,:,k) ), 2 );
+                        B(i,j,k) = norm( ( A(j,:,k) - A(i,:,k) ), 2 );
                     end
                 end
             end
-        end
-    
-        E = zeros(size(A,1),size(A,2),size(A,3));
-    
-        for k = 1:1:size(A,3)
-            for j = 1:1:size(dataSet,2)
-                for ii = 1:1:size(A,1)
+        
+            for k = 1:1:size(A,3) 
+                for j = 1:1:size(A,1)
                     for i = 1:1:size(A,1)
         
-                        if( i ~= ii )
+                        if( i ~= j )
         
-                            [ ~, E(i,j,k) ] = min(B(i,:,j,k),[],2); 
-                        end
-                        
+                            [ ~, E(j,k) ] = min(B(:,j,k),[],1); 
+                        end                    
+                    end
+                end           
+            end
+    
+            ii = 1;
+            for k = 1:1:size(A,3)
+                for j = 1:1:size(A,1)
+    
+                    V  = [ A(ii,:,k); A(E(j,k),:,k) ];
+                    V_ = sum(V,1)/2;
+        
+                    F(i,:,k) = V_; 
+                    
+                    A(E(j,k),:,k) = NaN; E(j,k) = NaN;   
+    
+                    ii = ii + 1;
+                end
+            end
+        
+            % We need a recusion on F to reduce the number of histograms to the
+            % number of classes being evaluated until we reach the convergence criterion...
+        
+            ll = 0; % Resize accumulator.
+        
+            for k = 1:1:size(A,3)
+                for j = 1:1:size(A,1)
+                    for i = 1:1:size(A,1)
+        
+                        B_(i,j,k) = norm( ( F(j,:,k) - F(i,:,k) ), 2 );
                     end
                 end
             end
-            V(j,k) = min(E(:,j,k),[],1);
-        end
-    
-        for k = 1:1:size(A,3)
-            for j = 1:1:size(dataSet,2)
-                for i = 1:1:size(A,1)
-        
-                    F(i,:,k) = ( A(i,:,k) + A(V(j,k),:,k) ) / 2; 
-                    
-                    A(V(j,k),:,k) = 0; V(j,k) = 0;   
-                end
-            end
-        end
-    
-        % We need a recusion on F to reduce the number of histograms to the
-        % number of classes being evaluated...
-    
-    
-        ll = 0; % Resize accumulator.
-    
-        for k = 1:1:size(dataSet,2)
-            for ii = 1:1:size(A,1)
-                for i = 1:1:size(A,1)
-    
-                    B_(i,ii,k) = norm( ( F(ii,:,k) - F(i,:,k) ), 2 );
-                end
-            end
-        end
-        
-        for k = 1:1:size(dataSet,2)
-            for ii = 1:1:size(A,1)
-                for i = 1:1:size(A,1)
-    
-                    if( i ~= ii )
-    
-                        [ ~, E_(i,k) ] = min(B_(i,:,k),[],2); 
-                    end
-                    
-                end
-            end
-            V_(k) = min(E(:,k),[],1);
-        end
-    
-         for k = 1:1:size(F,3)
-        
-            F(i,:,k) = ( F(i,:,k) + F_(V_(k),:,k) ) / 2; 
             
-            A(V_(k),:,k) = 0; V_(k) = 0; 
-         end
-    
-         for k = 1:1:size(F,3)
-    
-            F(V_(k),:,k) = 0;
-         end
-    
-         ll = ll + 1;
-         for k = 1:1:size(F,3)
-             for j = 1:1:size(F,2)
-                for i = 1:1:size(F,1)-ll
-    
-                    if ( F(i,j,k) ~= 0 )
-                        X(i,j,k) = F(i,j,k);
+            for k = 1:1:size(A,3) 
+                for j = 1:1:size(A,1)
+                    for i = 1:1:size(A,1)
+        
+                        if( i ~= j )
+        
+                            [ ~, E_(j,k) ] = min(B_(:,j,k),[],1); 
+                        end                    
                     end
-                end
+                end  
+                W(k) = min(E(:,k),[],1);
+            end
+        
+            for k = 1:1:size(F,3)
+            
+                F(i,:,k) = ( F(i,:,k) + F_(W(k),:,k) ) / 2; 
+                
+                A(W(k),:,k) = 0; 
+                
+                F(W(k),:,k) = 0;
+            end
+        
+             ll = ll + 1;
+             for k = 1:1:size(F,3)
+                 for j = 1:1:size(F,2)
+                    for i = 1:1:size(F,1)-ll
+        
+                        if ( F(i,j,k) ~= 0 && A(i,j,k) ~= 0 )
+    
+                            Q(i,j,k) = F(i,j,k);
+    
+                            P(i,j,k) = A(i,j,k);
+                        end
+                    end
+                 end
              end
-         end
-         F = 0; F = X;
+             F = 0; F = Q; A = 0; A = P;
+    
+         % Once we reach A = NxM, where N = M = number of classes, we still need to reduce A to
+         % zero...
+
+        elseif ( size(A,1) <= N && size(A,2) <= M )
 
 
 
 
-         
-
+        end
 
     end
     
