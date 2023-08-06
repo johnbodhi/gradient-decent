7cf393b7c053b7ed0d706c9bec5bfc43fcb21e10
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2023-08-06 12:11:22 -0700

    Update

--- histGradient_V1/MATLAB Code/frameSieve.m
+++ histGradient_V1/MATLAB Code/frameSieve.m
@@ -2,14 +2,9 @@ function [ Y ] = frameSieve( S )
 
     global RA
 
-    Y = S(:,1:end-1); Y = sort( Y,'descend' );
-
     for i = 1:1:size(RA,1)
-        for j = 1:1:size(RA,2)-1
             
-            Y(i,j) = S(1,j);      
-        end      
-        L(i,1) = i;
+        Y(i,:) = S(1,1:end-1); L(i,1) = i;
     end
 
     Y = cat(2,Y,L);    
