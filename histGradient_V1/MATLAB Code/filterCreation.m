ad7813f79556f1ad59cb8cb7e71653e28ef8dc72
johnbodhi <JMGarofalo@gmail.com>
2023-09-15 20:38:27 -0700

    Update

--- histGradient_V1/MATLAB Code/filterCreation.m
+++ histGradient_V1/MATLAB Code/filterCreation.m
@@ -21,7 +21,7 @@ function [ Y ] = filterCreation( dataSet )
 
         if( aa <= size(V,1) )
             
-            C = sum(dataSet(1:aa,:),2);
+            C = sum(dataSet(1:aa,:),1);
 
             Y(2,:,K) = Y(2,:,K) + C;
             Y        = Y / (aa*frameLength);
@@ -33,16 +33,13 @@ function [ Y ] = filterCreation( dataSet )
 
             dataSet = circshift(dataSet,1);
             
-            if( F1 >= 0.99 )
+            A(rr,1) = ACC; [ ~, M ] = max(A);
             
-                A(rr,1) = ACC; [ ~, M ] = max(A);
-                
-                X(:,:,:,rr) = Y; rr = rr + 1; break;            
-            end
+            X(:,:,:,rr) = Y; rr = rr + 1; 
 
         elseif( aa > size(V,1) && bb <= size(V,1) )
 
-            C =  sum(dataSet(1:bb,:),2);
+            C =  sum(dataSet(1:bb,:),1);
              
             Y(3,:,K)  = Y(3,:,K) + C; 
             Y         = Y / (bb*frameLength);
@@ -55,15 +52,13 @@ function [ Y ] = filterCreation( dataSet )
 
             dataSet = circshift(dataSet,1);
             
-            if( F1 >= 0.99 )
+            A(rr,1) = ACC; [ ~, M ] = max(A);
             
-                A(rr,1) = ACC; [ ~, M ] = max(A);
-                
-                X(:,:,:,rr) = Y; rr = rr + 1; break          
-            end
+            X(:,:,:,rr) = Y; rr = rr + 1; 
+
         elseif( bb > size(V,1) && cc <= size(V,1) )
 
-            C = sum(dataSet(1:cc,:),2);
+            C = sum(dataSet(1:cc,:),1);
              
             Y(4,:,K) = Y(4,:,K) + C; 
             Y        = Y / (cc*frameLength);
@@ -76,15 +71,13 @@ function [ Y ] = filterCreation( dataSet )
 
             dataSet = circshift(dataSet,1);
             
-            if( F1 >= 0.99 )
+            A(rr,1) = ACC; [ ~, M ] = max(A);
             
-                A(rr,1) = ACC; [ ~, M ] = max(A);
-                
-                X(:,:,:,rr) = Y; rr = rr + 1; break;             
-            end
+            X(:,:,:,rr) = Y; rr = rr + 1; 
+
         elseif( cc > size(V,1) && dd <= size(V,1) )
 
-            C = sum(dataSet(1:dd,:),2);
+            C = sum(dataSet(1:dd,:),1);
              
             Y(5,:,K) = Y(5,:,K) + C; 
             Y        = Y / (dd*frameLength);
@@ -96,15 +89,13 @@ function [ Y ] = filterCreation( dataSet )
 
             dataSet = circshift(dataSet,1);
             
-            if( F1 >= 0.99 )
+            A(rr,1) = ACC; [ ~, M ] = max(A);
             
-                A(rr,1) = ACC; [ ~, M ] = max(A);
-                
-                X(:,:,:,rr) = Y; rr = rr + 1; break;                
-            end
+            X(:,:,:,rr) = Y; rr = rr + 1; 
+
         elseif( dd > size(V,1) && ee <= size(V,1) )
 
-            C = sum(dataSet(1:ee,:),2);
+            C = sum(dataSet(1:ee,:),1);
              
             Y(6,:,K)  = Y(6,:,K) + C; 
             Y         = Y / (ee*frameLength); 
@@ -117,15 +108,13 @@ function [ Y ] = filterCreation( dataSet )
 
             dataSet = circshift(dataSet,1);
             
-            if( F1 >= 0.99 )
+            A(rr,1) = ACC; [ ~, M ] = max(A);
             
-                A(rr,1) = ACC; [ ~, M ] = max(A);
-                
-                X(:,:,:,rr) = Y; rr = rr + 1; break;              
-            end
+            X(:,:,:,rr) = Y; rr = rr + 1; 
+
         elseif( ee > size(V,1) && ff <= size(V,1) )
 
-            C = sum(dataSet(1:ff,:),2);
+            C = sum(dataSet(1:ff,:),1);
              
             Y(7,:,K) = Y(7,:,K) + C; 
             Y         = Y / (ff*frameLength); 
@@ -138,15 +127,13 @@ function [ Y ] = filterCreation( dataSet )
 
             dataSet = circshift(dataSet,1);
             
-            if( F1 >= 0.99 )
+            A(rr,1) = ACC; [ ~, M ] = max(A);
             
-                A(rr,1) = ACC; [ ~, M ] = max(A);
-                
-                X(:,:,:,rr) = Y; rr = rr + 1; break;                
-            end
+            X(:,:,:,rr) = Y; rr = rr + 1; 
+
         elseif( ff > size(V,1) && gg <= size(V,1) )
 
-            C = sum(dataSet(1:gg,:),2);
+            C = sum(dataSet(1:gg,:),1);
              
             Y(8,:,K) = Y(8,:,K) + C; 
             Y        = Y / (gg*frameLength); 
@@ -159,15 +146,13 @@ function [ Y ] = filterCreation( dataSet )
 
             dataSet = circshift(dataSet,1);
             
-            if( F1 >= 0.99 )
+            A(rr,1) = ACC; [ ~, M ] = max(A);
             
-                A(rr,1) = ACC; [ ~, M ] = max(A);
-                
-                X(:,:,:,rr) = Y; rr = rr + 1; break;                
-            end
+            X(:,:,:,rr) = Y; rr = rr + 1; 
+
         elseif( hh > size(V,1) )
 
-            C = sum(dataSet(1:hh,:),2);
+            C = sum(dataSet(1:hh,:),1);
              
             Y(9,:,K) = Y(9,:,K) + C; 
             Y        = Y / (hh*frameLength); 
@@ -180,12 +165,9 @@ function [ Y ] = filterCreation( dataSet )
 
             dataSet = circshift(dataSet,1);
             
-            if( F1 >= 0.99 )
+            A(rr,1) = ACC; [ ~, M ] = max(A);
             
-                A(rr,1) = ACC; [ ~, M ] = max(A);
-                
-                X(:,:,:,rr) = Y; rr = rr + 1; break;               
-            end
+            X(:,:,:,rr) = Y; rr = rr + 1; 
         end
         ii = ii + 1;
 
