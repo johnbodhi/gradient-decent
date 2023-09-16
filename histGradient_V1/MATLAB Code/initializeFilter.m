e6cfebb3e166ae1e10c494c41d0e77f8c65c0989
johnbodhi <JMGarofalo@gmail.com>
2023-09-16 00:53:03 -0700

    Update

--- histGradient_V1/MATLAB Code/initializeFilter.m
+++ histGradient_V1/MATLAB Code/initializeFilter.m
@@ -1,9 +1,11 @@
 function [ Y ] = initializeFilter( X )
+
+    global classType BINS classGroups
     
     RA  = zeros(size(classType,2)+1,BINS+1,size(classGroups,2)+1); 
 
-
     
+
     
     
 end
