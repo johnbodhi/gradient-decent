f15b40ae86b8cb27e50d55c01d5af3ef13f69d89
johnbodhi <JMGarofalo@gmail.com>
2023-09-26 18:03:21 -0700

    Update

--- histGradient_V1/MATLAB Code/BiCGSTAB.m
+++ histGradient_V1/MATLAB Code/BiCGSTAB.m
@@ -1,6 +1,6 @@
-function [ Z ] = BiCGSTAB( X, Y )
+function [ Z_ ] = BiCGSTAB( X_, Y_ )
 
-    global classType classGroups
+    global classType classGroups frameLength
 
     RA  = X;
 
@@ -10,55 +10,50 @@ function [ Z ] = BiCGSTAB( X, Y )
 
     SUM = size(classGroups,2) * N * M; 
     
-    B   = zeros(N,size(classGroups,2),size(classGroups,2));
-
-    R   = zeros(N,M,size(classGroups,2));
+    V   = zeros(N,size(classGroups,2),size(classGroups,2));
 
     % SUP = simpleNN(N,M); 
     
     UB  = 1004;
 
-    SUP = size(classGroups,2)*UB;
+    SUP = size(classGroups,2) * UB;
     
     ii = 1;
 
     aa = 1; bb = 1; 
 
-    while( sum(sum(sum(B,1),2),3) < SUM )
+    while( sum(sum(sum(V,1),2),3) < SUM )
 
         K = ceil( ii / SUP ) + 1;
 
-        if( aa <= size(B,2) )
+        if( aa <= size(V,2) )
 
-            B(aa,1,K) = 1;
+            V(aa,1,K) = 1;
 
             aa = aa + 1;    
             
+            A(:,1) = Y_(aa,:,K);            
             
+        elseif( aa > size(V,3) && bb <= size(V,3) )
 
-            
-        elseif( aa > size(B,3) && bb <= size(B,3) )
-
-            B(bb,2,K) = 1; B(:,1,K) = 0;
+            V(bb,2,K) = 1; V(:,1,K) = 0;
             
             bb = bb + 1; aa = 1;
             
-             
-            
-            
-        elseif( bb > size(B,1) && cc <= size(B,1) )
+            B(:,1) = frameLength * X_(bb,:,K);           
 
-            B(cc,3,K) = 1; B(:,2,K) = 0;
+        elseif( bb > size(V,1) && cc <= size(V,1) )
+
+            V(cc,3,K) = 1; V(:,2,K) = 0;
 
             cc = cc + 1; bb = 1;
             
-            
-            
+            X_(:,1) = Y_(cc,:,K);              
             
         end
 
 
-        
+
 
 
 
