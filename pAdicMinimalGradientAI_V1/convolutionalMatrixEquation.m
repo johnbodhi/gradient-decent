5395f4bf5d4194ada139f005756a8e575d38cd25
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2024-03-17 09:23:49 -0700

    Update convolutionalMatrixEquation.m

--- pAdicMinimalGradientAI_V1/convolutionalMatrixEquation.m
+++ pAdicMinimalGradientAI_V1/convolutionalMatrixEquation.m
@@ -1,6 +1,6 @@
 function [ Z_ ] = convolutionalMatrixEquation( Y_ )
 
-    global classType BINS frameLength TOL LIMIT
+    global classType BINS frameLength TOL LIMIT M
     
         
     N   = 0.5*size(Y_,1); 
@@ -10,7 +10,7 @@ function [ Z_ ] = convolutionalMatrixEquation( Y_ )
     
     CONTAINMENT = 0.5*N; % Objective function containment limit.
     
-    IT  = N*M-CONTAINMENT;
+    IT  = N^M-CONTAINMENT;
     
     V   = zeros( N, M );
     
@@ -71,60 +71,87 @@ function [ Z_ ] = convolutionalMatrixEquation( Y_ )
     % We need to sort the minimum walk accumulations in the 
     % form of a chart, and a transpose chart.
         
-    W_ = WALK_(:,:,:); 
+    W_ = WALK_( :, :, : ); 
     
-    [WA,LA] = sort(W_,2);
+    [ Z_ ] = averageMapping( W_ );
     
-    [WB,LB] = sort(W_,1);
-        
-    % We need to separate the walk accumulation deltas from eachother
-    % according to an average edge amplitude. This is the discrimination
-    % value for a class.
-            
-    EP_MU = 0.1; % We can  step through an epsilon vector as well once 
-                 % we have experience...
-        
-    RA = zeros(M,BINS); RB = zeros(M,BINS);
     
-    Z_ = zeros(size(W_,1),size(W_,2),size(W_,3),2);
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %{
+    
+    N   = 0.5*size(Y_,1); 
+    
+    M   = size(classType,2)+1;
+    
+    
+    CONTAINMENT = 0.5*N; % Objective function containment limit.
+    
+    IT  = N*M-CONTAINMENT;
+    
+    V   = zeros( N, M );
+    
+    
+    ii = 1; kk = 1;
+
+    aa = 1; bb = 1; cc = 1;
+
+    TOL = 1; LIMIT = 1e-2;
+    
         
-    ll = 0;
-    for k = 1:1:size(WALK_,3)
-        for j = 1:1:size(WALK_,2)
-            for i = 2:1:size(WALK_,1)
-                        
-                Z_(i-1,j,k,1) = WA(j,i,k) - WA(j,i-1,k);
-            
-                Z_(i-1,j,k,2) = WB(i,j,k) - WB(i-1,j,k);
-            
-                        
-                E(1,1) = Z_(i-1,j,k,1) / Z_(i,j,k,1);
+    X(:,1) = Y_(1,:); 
+    
+    F = sum(sum(V,1),2);
+    
+    while( F < IT )
+    
+        if( aa <= N )
+    
+            V(aa,1) = 1;
                     
-                E(1,2) = Z_(i-1,j,k,2) / Z_(i,j,k,2);
+            A(:,1) = Y_(aa,:);
             
+            RA_ = monteCarlo(Z_(cc,:,:));
                     
-                if( E(1,1) <= EP_MU && E(1,2) <= EP_MU )
-            
-                    RA(k,:) = RA(k,:) + Y_(LA(j,i-1,k),:);
-               
-                    RB(k,:) = RB(k,:) + Y_(LB(i-1,j,k),:);
-             
-                    ll = ll + 1;
-                end
+            B(:,1) = frameLength.*RA_(cc,:,1);
                 
-            end
-        end
+            WALK_(aa,bb,cc) = BiCGSTAB_(A,X,B);
+                
+            aa = aa + 1;
         
-        if( ll )
+        elseif( aa >= N && bb <= N )
+                
+            V(bb,2) = 1; V(:,1) = 0;
+    
+            X(:,1) = Y_(bb,:);
             
-            RA(k,:) = RA(k,:) / ll;
-           
-            RB(k,:) = RB(k,:) / ll;
+            RA_ = monteCarlo(Z_(cc,:,:));
+                    
+            B(:,1) = frameLength.*RA_(cc,:,1);
+                
+            WALK_(aa,bb,cc) = BiCGSTAB_(A,X,B);
+                
+            bb = bb + 1; aa = 1;
+        
+        elseif( bb >= N && cc <= CONTAINMENT )
+                
+            V(cc,3) = 1; V(:,2) = 0;
+                    
+            RA_ = monteCarlo(Z_(cc,:,:));
+                    
+            B(:,1) = frameLength.*RA_(cc,:,1);
+                
+            WALK_(aa,bb,cc) = BiCGSTAB_(A,X,B);
+                
+            cc = cc + 1; bb = 1;
             
-            ll = 0;
         end
+        F = sum(sum(V,1),2);
         
     end
-    Z_ = ( RA + RB ) ./ 2;
+    W_ = WALK_(:,:,:); 
+    
+    %}
+    
     
 end
