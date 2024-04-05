9ff812cddfade241e0bca69759ac6b3d68c4345f
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2024-04-04 20:32:32 -0700

    Update kmeans.m

--- pAdicMinimalGradientAI_V1/kmeans.m
+++ pAdicMinimalGradientAI_V1/kmeans.m
@@ -1,10 +1,10 @@
 function [ Y ] = kmeans( dataSet, l_ )
 
-global classGroups classType RA BPI BFLAG
+global RA BPI BFLAG
 
     S = dataSet(:,1:end-1,:);
 
-    V = size(RA,1)*size(dataSet,2);
+    V = size(BPI,2)*size(dataSet,2); N = 1;
 
     p = 2;
 
@@ -13,120 +13,68 @@ global classGroups classType RA BPI BFLAG
     % We need to weight our centroids, and backpropagate the
     % following gradient for every other class in a group.
 
-    for kk = 1:1:size(W,2)
-        for ii = 1:1:size(W,2)
-        
-            if ( ii == 1 )
+    for ii = 1:1:size(W,2)
     
-                W(ii,:,kk) = 1e0;
+        if ( ii == 1 )
 
-            elseif ( ii == 2 )
+            W(ii,:) = 1e0;
 
-                if( BFLAG )
-    
-                    W(ii,:,kk) = 1e0;
+        elseif ( ii == 2 )
 
-                elseif( ~BFLAG )
+            if( BFLAG )
 
-                    W(ii,:,kk) = 1e2; % Backprop...
-                    
-                end
-            end
-            
-        end
-    end
+                W(ii,:) = 1e0;
 
-    ii = 2; jj = 1; N = 1;
-    
-    while( jj < N*V ) % Modified convergence criterion.
+                BPI(1,1) = 0; 
 
-        if ( ii == N*V )
+            elseif( ~BFLAG )
 
-            ii = 2;
-        end
+                W(ii,:) = 1e2; % Backprop...
 
-        n = l_( ii, 1, 2 );
-       
-        % We need to mask the batch indexes.
-
-        B = zeros(1,1);
-        for j = 0:1:size(RA,1)-1
-            
-            B(1,j+1) = j;
-            
-            % B(1,j+1) = 2*j+1;
-
-            % B(2,j+1) = 2*(j+1);
-        end
-
-        for j = 1:1:size(B,2)
-            % for i = 1:1:size(B,1)
-
-                if( n ~= B(1,j) )
-                    
-                    B(1,j) = 0;
-                end
+                BPI(1,2) = 0; 
                 
-            % end
+            end
         end
-
-       TA = find( B(1,:) == n ); % TB = find( B(2,:) == n );
-
-       % if( TA )
-
-           % TB = 0;
-       % elseif( TB )
-
-           % TA = 0;
-       % end
-
-       % We can find all 2norms with the mask...
         
+    end
+  
+    for ii = 1:1:(N*V)  % Modified convergence criterion.
+
         for k = 1:1:size(BPI,2)
             for j = 1:1:size(S,2)
                 for i = 1:1:size(S,1)
             
-                    if ( TA )
+                    if ( BPI(1,1) )
             
                         D( i, j, k ) = ( ( S( i, j, k ) -...
-                            RA( TA , j, 2 ) )^p )^( 1 / p );  
+                            RA( BPI(1,1), j, 2 ) )^p )^( 1 / p );  
                         
-                    % elseif ( TB )
+                    elseif ( BPI(1,2) )
             
-                        % D( i, j, k ) = ( ( S( i, j, k ) -...
-                           % RA( TB, j, 2 ) )^p )^( 1 / p );
+                        D( i, j, k ) = ( ( S( i, j, k ) -...
+                            RA( BPI(1,2), j, 2 ) )^p )^( 1 / p );
                         
-                    end            
+                    end                    
                 end
             end
         
             Ci(k,1) = W(k,1,1) * mean(D(1,:,k)); 
             
-            % Cj(k,1) = W(k,2,1) * mean(D(2,:,k)); 
-            % 
-            % Ck(k,1) = W(k,3,1) * mean(D(3,:,k)); 
-            % 
-            % Cn(k,:) = [ Ci(k,1) Cj(k,1) Ck(k,1) ];
-            
             Cn(k,:) = [ Ci(k,1) ];        
         end       
-        
-        for bp = 1:1:size(BPI,2)
-            for i = 1:size(S,1)
-                for j = 1:size(S,2)
-    
-                    H( i, j, bp ) = ( ( S( i, j ) - ...
-                        Cn( bp, 1 ) )^p )^(1/p); 
-                end
-            end   
+         
+        for i = 2:size(S,1)
+            for j = 2:size(S,2)
+
+                H( i, j, 2 ) = ( ( S( i, j, 2 ) - ...
+                    Cn( i-1, 1 ) )^p )^(1/p); 
+            end
         end
+        Y = H(:,:,:); 
 
-        Y = H(:,:,1); Y = cat(1,Y,H(:,:,2));
-
-        ii = ii + 1; jj = jj + 1;
     end
     
-    for i = 2:1:size(l,1)
+    for i = 2:1:size(l_,1)
 
         Y(i,end,2) = l_(i, 1, 2); % Re-append labels for completeness...
     end
