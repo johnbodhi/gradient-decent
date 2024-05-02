4db1c53ba1ff9e2b321695b7036f8afcff00e986
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2024-05-02 10:13:50 -0700

    Update histPlay.m

--- pAdicMinimalGradientAI_V1/histPlay.m
+++ pAdicMinimalGradientAI_V1/histPlay.m
@@ -8,20 +8,19 @@ frameLength = 1250; classType = [0 0]; classGroups = [0 0 0];
 
 DATARANGE = 256; BINS = 16; Randomized = 0; Supervision = 0;
 
-N = 16;
+% Objective labels.
 
-% We can read in supervised or unsupervised n-dimensional data.
+l = [ 1; 1; 1; 1; 2; 2; 2; 2; 3; 3; 3; 3; 4; 4; 4; 4 ]; N = 16;
 
-A  = readmatrix('trainRGB (4).csv');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% We want to homogenize the data, and organize the samples 
-% symmetrically or asymmetrically into a single array.
+A  = readmatrix('trainRGB (1).csv');
 
 A_ = histogramization(A,N,[]); A_ = mean(A_(:,:,:),3);
 
 % A_ = complexHistogramization(A,N,[]); 
 
-DELTA = fHMM(A_);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 C_ = zeros(size(A_,1),size(A_,2),size(A_,3));
 
@@ -34,9 +33,6 @@ for k =1:1:size(A_,3)
    end
 end
 
-% We can look at the sorted turbulence of our hypersurfaces, 
-% as well as the column indexes. This is a random forest.
-
 for k = 1:1:size(A_,3)
     
    G(:,:,k)  = C_(:,:,k); 
@@ -44,103 +40,26 @@ for k = 1:1:size(A_,3)
    Gj(:,:,k) = D_(:,:,k);
 end
 
-% We need to boost our filter allocation to have globally 
-% defined values in our learning rate.
-
-% We can pass the sample set into a convolutional version of 
-% krylov subspace for a single filter update with a pre-existing filter,
-% or we can generate an initial condition for a supergroup of classes,
-% and proceed with further sample sets with the same method or with
-% gradient decent. This is error correction.
-
-% These are objective labels, however the data may or may not be labeled.
-
-l = [ 1; 1; 1; 1; 2; 2; 2; 2; 3; 3; 3; 3; 4; 4; 4; 4 ]; % Optional labels...
-
-% A_ = monteCarlo(A);
-
-% We need to define initial conditions of our filter without knowing 
-% what we are sampling, with or without supervision. For this, we use
-% a convolutional sub-gradient process to exhaust all combinations of
-% the samples in the averages before classifying against the same data
-% for which the filter is being composed. We also continually optimize 
-% the filter by deleting unecessary elements belonging to the
-% seperate categories, because this saves time and space in the future.
-
-% Now, we need to back propagate on the same data, and in the same
-% structure, to amend the filter manifold with a new category. 
-% This allows us to extend our range of classifications are far as 
-% necessary. 
-
-% We can contiunally optimize the embedding as we introduce 
-% new data, and extend our filter manifold to infinity. 
-
-% We also introduce error smoothing in a p-adic tree structure 
-% in the event the construction failes to converge properly by 
-% failing to avoid repetition in the averages, or yield results
-% that depend on overly exclusive averages.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 BOOST = 1;
 
 RA = zeros(size(classType,2)+BOOST,BINS+BOOST,size(classGroups,2)-2+BOOST);
 
-B_ = size(classGroups,2); % Convergence criterion.
-
-A_ = mean(A_(:,:,:),3);
-
-[ RA, TL_ ] = buildManifold( A_, l ); 
+[ RA, TL_ ] = CNN( A_, l ); 
 
 [ RA ] = filterOptimization(RA, A_, l );
 
-% NN = sum((1:1:N));
-
-%{
-
-while( B_ )
-    
-    RA = cat(1,RA,zeros(size(classType,2),BINS,size(classGroups,2)));
-
-    [ RA, TL_ ] = buildManifold( A_, l ); 
-    
-    for k = 1:1:size(classGroups,2)
-        
-        if( (find(TL_(:,1,k) == TL_(:,2,k))) &&...
-                ( ((sum(TL_(:,1,k) + sum((TL_(:,2,k)) < NN ) ||...
-                ( ( sum(TL_(:,1,k) + sum((TL_(:,2,k)) > NN) ) )
-            
-            % We need to continually inject new data 
-            % into suspicious categories that causes row-wise
-            % samples to be unused or used repetitively in a binary 
-            % categorification.
-            
-            % [ RA, TL_ ] = decisionTree();
-        else
-            
-            % B_ = B_ - 1; % Color
-        end
-    end
-end
-
-[ RA ] = filterOptimization(RA, A_, l ); 
-
-%}
-
-% We now have a preconditioned, or optimized, array that we can 
-% send into Krylov Subspace. This may hasten verification, or
-% classification of new data.
-
-%{
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 BOOST = 0;
 
 RA = zeros(size(classType,2)+BOOST,BINS+BOOST,size(classGroups,2)+BOOST);
 
-% RA = RA(2:end,2:end,2:end);
+[ RA ] = GMRES( A_ );
 
-A_ = mean(A_(:,:,:),3);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[ RA ] = convolutionalMatrixEquation( A_ );
-
-%}
+DELTA = fHMM(A_);
 
 toc
